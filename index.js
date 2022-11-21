26d07c508f179131ef326ac32a650008f46cc7f3
James Moura <35638433+grupomoura@users.noreply.github.com>
2022-11-21 02:12:35 -0300

    Update index.js

--- index.js
+++ index.js
@@ -23,15 +23,15 @@ app.get("/sinopsesOverview", (req,res) => {
 })
 
 app.get("/sinopsesOverview/theBook", (req,res) => {
-    return res.json(sinopsesOverview.sinopsesOverview.theBook)
+    return res.json(sinopsesOverview.sinopsesOverview[0])
 })
 
 app.get("/sinopsesOverview/oldTestment", (req,res) => {
-    return res.json(sinopsesOverview.sinopsesOverview.oldTestment)
+    return res.json(sinopsesOverview.sinopsesOverview[1])
 })
 
 app.get("/sinopsesOverview/newTestment", (req,res) => {
-    return res.json(sinopsesOverview.sinopsesOverview.newTestment)
+    return res.json(sinopsesOverview.sinopsesOverview[2])
 })
 
 app.get("/stories", (req,res) => {
