dfa5d469fb2a02c25a0f54a1916d441d4b884b8e
James Moura <35638433+grupomoura@users.noreply.github.com>
2022-11-21 03:11:26 -0300

    Update index.js

--- index.js
+++ index.js
@@ -26,6 +26,7 @@ app.get("/sinopsesOverview/theBook", (req,res) => {
     return res.json(sinopsesOverview.sinopsesOverview[0])
 })
 
+// Sinopse e overview do antigo testamento
 app.get("/sinopsesOverview/oldTestment", (req,res) => {
     return res.json(sinopsesOverview.sinopsesOverview[1])
 })
@@ -33,11 +34,203 @@ app.get("/sinopsesOverview/oldTestment", (req,res) => {
 app.get("/sinopsesOverview/oldTestment/gen", (req,res) => {
     return res.json(sinopsesOverview.sinopsesOverview[1][1])
 })
-
-app.get("/sinopsesOverview/newTestment", (req,res) => {
-    return res.json(sinopsesOverview.sinopsesOverview[2])
+app.get("/sinopsesOverview/oldTestment/exo", (req,res) => {
+    return res.json(sinopsesOverview.sinopsesOverview[2][1])
+})
+app.get("/sinopsesOverview/oldTestment/lev", (req,res) => {
+    return res.json(sinopsesOverview.sinopsesOverview[1][3])
+})
+app.get("/sinopsesOverview/oldTestment/num", (req,res) => {
+    return res.json(sinopsesOverview.sinopsesOverview[1][4])
+})
+app.get("/sinopsesOverview/oldTestment/deu", (req,res) => {
+    return res.json(sinopsesOverview.sinopsesOverview[1][5])
+})
+app.get("/sinopsesOverview/oldTestment/jos", (req,res) => {
+    return res.json(sinopsesOverview.sinopsesOverview[1][6])
+})
+app.get("/sinopsesOverview/oldTestment/jdg", (req,res) => {
+    return res.json(sinopsesOverview.sinopsesOverview[1][7])
+})
+app.get("/sinopsesOverview/oldTestment/rut", (req,res) => {
+    return res.json(sinopsesOverview.sinopsesOverview[1][8])
+})
+app.get("/sinopsesOverview/oldTestment/1sa", (req,res) => {
+    return res.json(sinopsesOverview.sinopsesOverview[1][9])
+})
+app.get("/sinopsesOverview/oldTestment/2sa", (req,res) => {
+    return res.json(sinopsesOverview.sinopsesOverview[1][10])
+})
+app.get("/sinopsesOverview/oldTestment/1ki", (req,res) => {
+    return res.json(sinopsesOverview.sinopsesOverview[1][11])
+})
+app.get("/sinopsesOverview/oldTestment/2ki", (req,res) => {
+    return res.json(sinopsesOverview.sinopsesOverview[1][12])
+})
+app.get("/sinopsesOverview/oldTestment/1ch", (req,res) => {
+    return res.json(sinopsesOverview.sinopsesOverview[1][13])
+})
+app.get("/sinopsesOverview/oldTestment/2ch", (req,res) => {
+    return res.json(sinopsesOverview.sinopsesOverview[1][14])
+})
+app.get("/sinopsesOverview/oldTestment/ezr", (req,res) => {
+    return res.json(sinopsesOverview.sinopsesOverview[1][15])
+})
+app.get("/sinopsesOverview/oldTestment/neh", (req,res) => {
+    return res.json(sinopsesOverview.sinopsesOverview[1][16])
+})
+app.get("/sinopsesOverview/oldTestment/job", (req,res) => {
+    return res.json(sinopsesOverview.sinopsesOverview[1][17])
+})
+app.get("/sinopsesOverview/oldTestment/psa", (req,res) => {
+    return res.json(sinopsesOverview.sinopsesOverview[1][18])
+})
+app.get("/sinopsesOverview/oldTestment/pro", (req,res) => {
+    return res.json(sinopsesOverview.sinopsesOverview[1][19])
+})
+app.get("/sinopsesOverview/oldTestment/ecc", (req,res) => {
+    return res.json(sinopsesOverview.sinopsesOverview[1][20])
+})
+app.get("/sinopsesOverview/oldTestment/sng", (req,res) => {
+    return res.json(sinopsesOverview.sinopsesOverview[1][21])
+})
+app.get("/sinopsesOverview/oldTestment/isa", (req,res) => {
+    return res.json(sinopsesOverview.sinopsesOverview[1][22])
+})
+app.get("/sinopsesOverview/oldTestment/ger", (req,res) => {
+    return res.json(sinopsesOverview.sinopsesOverview[1][23])
+})
+app.get("/sinopsesOverview/oldTestment/lam", (req,res) => {
+    return res.json(sinopsesOverview.sinopsesOverview[1][24])
+})
+app.get("/sinopsesOverview/oldTestment/ezk", (req,res) => {
+    return res.json(sinopsesOverview.sinopsesOverview[1][25])
+})
+app.get("/sinopsesOverview/oldTestment/dan", (req,res) => {
+    return res.json(sinopsesOverview.sinopsesOverview[1][26])
+})
+app.get("/sinopsesOverview/oldTestment/hos", (req,res) => {
+    return res.json(sinopsesOverview.sinopsesOverview[1][27])
+})
+app.get("/sinopsesOverview/oldTestment/jol", (req,res) => {
+    return res.json(sinopsesOverview.sinopsesOverview[1][28])
+})
+app.get("/sinopsesOverview/oldTestment/amo", (req,res) => {
+    return res.json(sinopsesOverview.sinopsesOverview[1][29])
+})
+app.get("/sinopsesOverview/oldTestment/jon", (req,res) => {
+    return res.json(sinopsesOverview.sinopsesOverview[1][30])
+})
+app.get("/sinopsesOverview/oldTestment/mic", (req,res) => {
+    return res.json(sinopsesOverview.sinopsesOverview[1][31])
+})
+app.get("/sinopsesOverview/oldTestment/nam", (req,res) => {
+    return res.json(sinopsesOverview.sinopsesOverview[1][32])
+})
+app.get("/sinopsesOverview/oldTestment/hab", (req,res) => {
+    return res.json(sinopsesOverview.sinopsesOverview[1][33])
+})
+app.get("/sinopsesOverview/oldTestment/zep", (req,res) => {
+    return res.json(sinopsesOverview.sinopsesOverview[1][34])
+})
+app.get("/sinopsesOverview/oldTestment/hag", (req,res) => {
+    return res.json(sinopsesOverview.sinopsesOverview[1][35])
+})
+app.get("/sinopsesOverview/oldTestment/zec", (req,res) => {
+    return res.json(sinopsesOverview.sinopsesOverview[1][36])
+})
+app.get("/sinopsesOverview/oldTestment/mal", (req,res) => {
+    return res.json(sinopsesOverview.sinopsesOverview[1][37])
 })
 
+
+// Sinopse e overview do novo testamento
+app.get("/sinopsesOverview/newTestment/mat", (req,res) => {
+    return res.json(sinopsesOverview.sinopsesOverview[2][1])
+})
+app.get("/sinopsesOverview/newTestment/mrk", (req,res) => {
+    return res.json(sinopsesOverview.sinopsesOverview[2][2])
+})
+app.get("/sinopsesOverview/newTestment/luk", (req,res) => {
+    return res.json(sinopsesOverview.sinopsesOverview[2][3])
+})
+app.get("/sinopsesOverview/newTestment/jhn", (req,res) => {
+    return res.json(sinopsesOverview.sinopsesOverview[2][4])
+})
+app.get("/sinopsesOverview/newTestment/act", (req,res) => {
+    return res.json(sinopsesOverview.sinopsesOverview[2][5])
+})
+app.get("/sinopsesOverview/newTestment/rom", (req,res) => {
+    return res.json(sinopsesOverview.sinopsesOverview[2][6])
+})
+app.get("/sinopsesOverview/newTestment/1co", (req,res) => {
+    return res.json(sinopsesOverview.sinopsesOverview[2][7])
+})
+app.get("/sinopsesOverview/newTestment/2co", (req,res) => {
+    return res.json(sinopsesOverview.sinopsesOverview[2][8])
+})
+app.get("/sinopsesOverview/newTestment/gal", (req,res) => {
+    return res.json(sinopsesOverview.sinopsesOverview[2][9])
+})
+app.get("/sinopsesOverview/newTestment/eph", (req,res) => {
+    return res.json(sinopsesOverview.sinopsesOverview[2][10])
+})
+app.get("/sinopsesOverview/newTestment/php", (req,res) => {
+    return res.json(sinopsesOverview.sinopsesOverview[2][11])
+})
+app.get("/sinopsesOverview/newTestment/col", (req,res) => {
+    return res.json(sinopsesOverview.sinopsesOverview[2][12])
+})
+app.get("/sinopsesOverview/newTestment/1th", (req,res) => {
+    return res.json(sinopsesOverview.sinopsesOverview[2][13])
+})
+app.get("/sinopsesOverview/newTestment/2th", (req,res) => {
+    return res.json(sinopsesOverview.sinopsesOverview[2][14])
+})
+app.get("/sinopsesOverview/newTestment/1ti", (req,res) => {
+    return res.json(sinopsesOverview.sinopsesOverview[2][15])
+})
+app.get("/sinopsesOverview/newTestment/2ti", (req,res) => {
+    return res.json(sinopsesOverview.sinopsesOverview[2][16])
+})
+app.get("/sinopsesOverview/newTestment/tit", (req,res) => {
+    return res.json(sinopsesOverview.sinopsesOverview[2][17])
+})
+app.get("/sinopsesOverview/newTestment/phm", (req,res) => {
+    return res.json(sinopsesOverview.sinopsesOverview[2][18])
+})
+app.get("/sinopsesOverview/newTestment/heb", (req,res) => {
+    return res.json(sinopsesOverview.sinopsesOverview[2][19])
+})
+app.get("/sinopsesOverview/newTestment/jas", (req,res) => {
+    return res.json(sinopsesOverview.sinopsesOverview[2][20])
+})
+app.get("/sinopsesOverview/newTestment/1pe", (req,res) => {
+    return res.json(sinopsesOverview.sinopsesOverview[2][21])
+})
+app.get("/sinopsesOverview/newTestment/2pe", (req,res) => {
+    return res.json(sinopsesOverview.sinopsesOverview[2][22])
+})
+app.get("/sinopsesOverview/newTestment/1jn", (req,res) => {
+    return res.json(sinopsesOverview.sinopsesOverview[2][23])
+})
+app.get("/sinopsesOverview/newTestment/2jn", (req,res) => {
+    return res.json(sinopsesOverview.sinopsesOverview[2][24])
+})
+app.get("/sinopsesOverview/newTestment/3jn", (req,res) => {
+    return res.json(sinopsesOverview.sinopsesOverview[2][25])
+})
+app.get("/sinopsesOverview/newTestment/jud", (req,res) => {
+    return res.json(sinopsesOverview.sinopsesOverview[2][26])
+})
+app.get("/sinopsesOverview/newTestment/rev", (req,res) => {
+    return res.json(sinopsesOverview.sinopsesOverview[2][27])
+})
+
+
+
+
+// Histórias bíblicas ilustradas
 app.get("/stories", (req,res) => {
     return res.json(stories)
 })
